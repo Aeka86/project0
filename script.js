0111d287ae8e63532c09b43e127540ce0e0a5f88
AikaS <aykusha86@gmail.com>
2023-03-31 13:52:54 +0600

    trying to figure out on how to create a to do list

--- script.js
+++ script.js
@@ -10,5 +10,17 @@ const itemCountSpan = document.getElementById('item-count')
 const uncheckedCountSpan = document.getElementById('unchecked-count')
 
 function newTodo() {
-  alert('New TODO button clicked!')
+  var todoList = document.getElementById("todo-list");
+  var todoItem = document.createElement("li");
+  var todoText = document.createTextNode("New TODO item");
+  todoItem.appendChild(todoText);
+  todoList.appendChild(todoItem);
+  var itemCount = document.getElementById("item-count");
+  itemCount.textContent = parseInt(itemCount.textContent) + 1;
+  
+  var uncheckedCount = document.getElementById("unchecked-count");
+  uncheckedCount.textContent = parseInt(uncheckedCount.textContent) + 1;
+  
 }
+
+
