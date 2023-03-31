54e0fbec8a5f1b87e7178f6a1db81ae729b57664
AikaS <aykusha86@gmail.com>
2023-03-31 16:03:31 +0600

    done right

--- script.js
+++ script.js
@@ -9,20 +9,42 @@ const list = document.getElementById('todo-list')
 const itemCountSpan = document.getElementById('item-count')
 const uncheckedCountSpan = document.getElementById('unchecked-count')
 
+let todoCount = 0;
+let uncheckedCount = 0;
+
 function addTodo() {
   const todoList = document.getElementById("todo-list");
-  const todoInput = prompt("Enter a new TODO:");
-  if (todoInput) {
-    const newTodo = document.createElement("li");
-    newTodo.innerHTML = `
-      <span>${todoInput}</span>
-      <button class="delete-button">Delete</button>
-    `;
-    todoList.appendChild(newTodo);
-    updateItemCount();
+  const todoInput = prompt("Enter your TODO:");
+  if (todoInput !== null && todoInput.trim() !== "") {
+    const todoItem = document.createElement("li");
+    const todoText = document.createElement("span");
+    const deleteBtn = document.createElement("button");
+
+    todoText.innerText = todoInput;
+    todoItem.appendChild(todoText);
+
+    deleteBtn.innerText = "Delete";
+    deleteBtn.addEventListener("click", function () {
+      todoItem.remove();
+      todoCount--;
+      uncheckedCount--;
+      updateCounts();
+    });
+    todoItem.appendChild(deleteBtn);
+
+    todoList.appendChild(todoItem);
+    todoCount++;
+    uncheckedCount++;
+    updateCounts();
   }
 }
 
-  
+function updateCounts() {
+  document.getElementById("item-count").innerText = todoCount;
+  document.getElementById("unchecked-count").innerText = uncheckedCount;
+}
 
+document.getElementById("new-todo-btn").addEventListener("click", function () {
+  addTodo();
+});
 
